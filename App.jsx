d1d6427b3bc75e3e64bbbc275c6bb9c778944a17
Vindetta100 <134486376+Vindetta100@users.noreply.github.com>
2025-10-14 11:56:16 -0400

    Fix: Restore original site at base URL, keep 5 variations at /1-/5

--- App.jsx
+++ App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Original from './src/versions/original/Home'
 import V1 from './src/versions/v1/Home'
 import V2 from './src/versions/v2/Home'
 import V3 from './src/versions/v3/Home'
@@ -10,7 +11,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<V1 />} />
+        <Route path="/" element={<Original />} />
         <Route path="/1" element={<V1 />} />
         <Route path="/2" element={<V2 />} />
         <Route path="/3" element={<V3 />} />
@@ -22,3 +23,4 @@ function App() {
 }
 
 export default App
+
